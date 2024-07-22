57f7448ae9d3f32f76c1a7a6907e81ff97dc7e28
vantu8386 <nguyenvantu131197@gmail.com>
2024-07-22 11:43:45 +0700

    tsh

--- public/hat-nhua/js/list-posts.js
+++ public/hat-nhua/js/list-posts.js
@@ -83,6 +83,41 @@ const files = [
     files_href: "nhua-abs-la-gi.html",
     name: "Nhựa ABS là gì",
   },
+  {
+    id: 7,
+    files_href: "bio-ca-phe-pp.html",
+    name: "BIO cà phê PP",
+  },
+  {
+    id: 8,
+    files_href: "bio-go-pp.html",
+    name: "BIO Gỗ PP",
+  },
+  {
+    id: 9,
+    files_href: "bio-rom-abs.html",
+    name: "BIO Rơm ABS",
+  },
+  {
+    id: 10,
+    files_href: "bio-rom-pe.html",
+    name: "BIO Rơm PE",
+  },
+  {
+    id: 11,
+    files_href: "bio-rom-pla.html",
+    name: "BIO Rơm PLA",
+  },
+  {
+    id: 11,
+    files_href: "bio-rom-pp.html",
+    name: "BIO Rơm PP",
+  },
+  {
+    id: 12,
+    files_href: "bio-tre-pp.html",
+    name: "BIO Tre PP",
+  },
 ];
 
 const shownFiles = new Set(); // Sử dụng Set để kiểm tra nhanh hơn
